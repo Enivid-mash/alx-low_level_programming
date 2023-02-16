9e1d903f5bfa623d78699568713851a54cadf351
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-02-16 15:55:12 -0800

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* int main - prints "Programming is like building a multilingual puzzle
+* return: returns 0
+*/
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
