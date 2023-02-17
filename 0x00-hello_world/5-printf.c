eb86a05e1ea6813a387ebaf07b427eb1672c3d32
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-02-16 17:24:28 -0800

    Add 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - Prints 'the program's prints a value by using the put variable'
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
