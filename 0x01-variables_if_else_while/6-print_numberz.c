789a35c09bc4942afb036c7c469d05963cf7273b
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-02-19 02:05:12 -0800

    Add 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers from 0 to 9
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
