09ab8d710d19fdc78868463cf706db85f9c45f29
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-03-07 07:15:53 -0800

    Add 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -26,3 +26,24 @@ char *_strstr(char *haystack, char *needle)
 		}
 	return ((void *)0);
 }
+
+/**
+ * _strchr - a function that locates a character in a string
+ *
+ * @s: the string to search
+ * @c: the character to find
+ *
+ * Return: a pointer to the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	const char ch = c;
+
+	for (; *s != ch; ++s)
+	{
+		if (*s == '\0')
+			return ((void *)0);
+	}
+	return ((char *)s);
+}
+
