4450b36da28c619cbc72a8f7b1b7ca277aba8f4f
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-03-21 16:49:33 -0700

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
