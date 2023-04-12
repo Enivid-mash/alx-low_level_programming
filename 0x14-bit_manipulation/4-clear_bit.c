603935aa9a6a9ff0e0010d5bcfe91907e6725422
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-04-11 18:27:32 -0700

    Add 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,6 +12,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int i = 0x01;
 
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
 	i = ~(i << index);
 	if (i == 0x00)
 		return (-1);
