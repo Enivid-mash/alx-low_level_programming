54eae553b15ea15ae3b1bba1a9631875653f25aa
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-04-14 17:20:29 -0700

    Add 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,61 +5,58 @@
 
 /**
  * main - copies the content of a file to another.
- *
  * @argc: argument count
  * @argv: arguments
- *
  * Return: 1 on success, exit otherwise
  */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
-		exit(1); /* Exit with error code 1 */
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(1);
 	}
 	int src = open(argv[1], O_RDONLY);
 
 	if (src == -1)
 	{
-		perror("Error opening source file");
-		exit(2); /* Exit with error code 2 */
+		dprintf(STDERR_FILENO, "Error opening source file\n");
+		exit(2);
 	}
 	int dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 
 	if (dest == -1)
 	{
-		perror("Error opening destination file");
-		exit(3); /* Exit with error code 3 */
+		dprintf(STDERR_FILENO, "Error opening destination file\n");
+		exit(3);
 	}
-	char buffer[1024]; /* Buffer to hold data */
-	ssize_t n_read;    /* Number of bytes read from source file */
-	ssize_t n_written; /* Number of bytes written to destination file */
+	char buffer[1024];
+	ssize_t n_read, ssize_t n_written;
 
 	while ((n_read = read(src, buffer, sizeof(buffer))) > 0)
 	{
 		n_written = write(dest, buffer, n_read);
 		if (n_written != n_read)
 		{
-			perror("Error writing to destination file");
-			exit(4); /* Exit with error code 4 */
+			dprintf(STDERR_FILENO, "Error writing to destination file\n");
+			exit(4);
 		}
 	}
 	if (n_read == -1)
 	{
-		perror("Error reading from source file");
-		exit(5); /* Exit with error code 5 */
+		dprintf(STDERR_FILENO, "Error reading from source file\n");
+		exit(5);
 	}
-
 	if (close(src) == -1)
 	{
-		perror("Error closing source file");
-		exit(6); /* Exit with error code 6 */
+		dprintf(STDERR_FILENO, "Error closing source file\n");
+		exit(6);
 	}
 	if (close(dest) == -1)
 	{
-		perror("Error closing destination file");
-		exit(7); /* Exit with error code 7 */
+		dprintf(STDERR_FILENO, "Error closing destination file\n");
+		exit(7);
 	}
-	return (0); /* Exit with success status */
+	return (0);
 }
+
