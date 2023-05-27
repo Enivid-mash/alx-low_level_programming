34ad7f361d54705c91c7ceda8e122cbf80314351
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-05-26 17:36:25 -0700

    Add 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o /tmp/mawunyo.so https://github.com/Enivid-mash/alx-low_level_programming/blob/master/0x18-dynamic_libraries/mawunyo.so
+wget -P /tmp https://github.com/Enivid-mash/alx-low_level_programming/blob/master/0x18-dynamic_libraries/mawunyo.so
 export LD_PRELOAD=/tmp/mawunyo.so
