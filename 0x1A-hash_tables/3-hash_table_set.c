8495c52d524450d841038341a65b9d1f3767d0ff
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-06-18 07:22:42 -0700

    Add 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -37,7 +37,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	new_node->next = ht->array[index];
+	new_node->next = NULL;
+
+	if (ht->array[index] != NULL)
+		new_node->next = ht->array[index];
+
 	ht->array[index] = new_node;
 
 	return (1);
