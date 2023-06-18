fc49d8b419ac11944e5b10a41a37c1970cb22085
Mawunyo on axl sandbox <dmashigbi@gmail.com>
2023-06-18 07:05:30 -0700

    Add 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,11 +15,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
-
 
 	if (new_node == NULL)
+		return (0);
+
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	new_node->key = strdup(key);
